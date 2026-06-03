d3474334874a5ce7604fa8e26b92799161cca2c8
vikuvikas1622 <vikuvikas76@gmail.com>
2026-06-03 16:22:46 +0530

    Add ZIP extraction and folder creation logic
    Added functionality to create visuals and data folders, and extract a ZIP file.

--- notebooks/analysis.py
+++ notebooks/analysis.py
@@ -10,3 +10,26 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# -----------------------------------------
+# Create visuals folder if not exists
+# -----------------------------------------
+
+os.makedirs("../visuals", exist_ok=True)
+
+# -----------------------------------------
+# Extract ZIP File
+# -----------------------------------------
+
+zip_path = "../archive.zip"
+
+extract_path = "../data"
+
+# Create data folder if not exists
+os.makedirs(extract_path, exist_ok=True)
+
+# Unzip archive.zip
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extractall(extract_path)
+
+print("\n========== ZIP FILE EXTRACTED ==========\n")
